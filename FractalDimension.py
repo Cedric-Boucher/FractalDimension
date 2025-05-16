839765d84a2e7b02d69ba8a55d86c069c78c0306
Cedric Boucher <cjbouche@ualberta.ca>
2025-05-16 13:55:20 -0600

    add assertions and type hints to help ensure proper functioning

--- FractalDimension.py
+++ FractalDimension.py
@@ -12,31 +12,34 @@ def fractal_dimension(image: np.ndarray) -> np.float64:
     Returns:
         D: The fractal dimension Df, as estimated by the modified box-counting algorithm.
     """
-    M = image.shape[0]  # image shape
-    G_min = image.min()  # lowest gray level (0=white)
-    G_max = image.max()  # highest gray level (255=black)
-    G = G_max - G_min + 1  # number of gray levels, typically 256
-    prev = -1  # used to check for plateaus
-    r_Nr = []
+    assert len(image.shape) == 2, f"Image must be 2-dimensional, but it was {len(image.shape)}-dimensional"
+    assert image.shape[0] == image.shape[1], f"Image must be square, but dimensions were ({image.shape[0]}, {image.shape[1]})"
+    assert isinstance(image.max(), int), f"Image must be made of integer values, but was made of {type(image.max()).__name__}"
+    M: int = image.shape[0]  # image shape
+    G_min: int = image.min()  # lowest gray level (0=white)
+    G_max: int = image.max()  # highest gray level (255=black)
+    G: int = G_max - G_min + 1  # number of gray levels, typically 256
+    prev: int = -1  # used to check for plateaus
+    r_Nr: list[tuple[float, int]] = []
 
     for L in range(2, (M // 2) + 1):
-        h = max(1, G // (M // L))  # minimum box height is 1
-        N_r = 0
-        r = L / M
+        h: int = max(1, G // (M // L))  # minimum box height is 1
+        N_r: int = 0
+        r: float = L / M
         for i in range(0, M, L):
-            boxes = [[]] * ((G + h - 1) // h)  # create enough boxes with height h to fill the fractal space
+            boxes: list[list[float]] = [[]] * ((G + h - 1) // h)  # create enough boxes with height h to fill the fractal space
             for row in image[i:i + L]:  # boxes that exceed bounds are shrunk to fit
                 for pixel in row[i:i + L]:
-                    height = (pixel - G_min) // h  # lowest box is at G_min and each is h gray levels tall
+                    height: int = (pixel - G_min) // h  # lowest box is at G_min and each is h gray levels tall
                     boxes[height].append(pixel)  # assign the pixel intensity to the correct box
             stddev = np.sqrt(np.var(boxes, axis=1))  # calculate the standard deviation of each box
             stddev = stddev[~np.isnan(stddev)]  # remove boxes with NaN standard deviations (empty)
             nBox_r = 2 * (stddev // h) + 1
             N_r += sum(nBox_r)
         if N_r != prev:  # check for plateauing
-            r_Nr.append([r, N_r])
+            r_Nr.append((r, N_r))
             prev = N_r
-    x = np.array([np.log(1 / point[0]) for point in r_Nr])  # log(1/r)
-    y = np.array([np.log(point[1]) for point in r_Nr])  # log(Nr)
+    x = np.array(np.log(1/r) for r, _ in r_Nr)  # log(1/r)
+    y = np.array(np.log(N_r) for _, N_r in r_Nr)  # log(Nr)
     D = np.polyfit(x, y, 1)[0]  # D = lim r -> 0 log(Nr)/log(1/r)
     return D
